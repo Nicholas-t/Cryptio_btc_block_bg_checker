e0362f87680a28fd84ae9c056b56d406e187ec8d
Nicholas-t <nicholas-t.setijabudiharsa@polytechnique.edu>
2019-07-22 12:08:34 +0200

    fix test

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ block_hash = recent_hash()
 
 print(block_hash)
 
+
+
 txn = block_search(("91.121.170.214", 8333),
 				block_hash,
                 target_unspent_tx="82e63e889759bccc0561d771c657fb0e451bd3b750d5735535f8c770891c1a46")
