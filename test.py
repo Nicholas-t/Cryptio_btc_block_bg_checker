3e23efc1b4a0fa89393e59c76601b7ac7b014acd
nicholas-t <nicholas-t.setijabudiharsa@polytechnique.edu>
2019-07-29 16:13:46 +0000

    fix for demo

--- test.py
+++ test.py
@@ -1,21 +1,18 @@
 from bitcoin_block_bg_checker.main import block_search, recent_hash
 from time import time
-import psutil
 
-
-current_process = psutil.Process()
 
 block_hash = recent_hash()
 
 print(block_hash)
+#550067 blocks
 
+block_hash = '000000000000000000161a4d8d05f96dda16d23262a3540c39c4365b38f1c1f8'
 now1 = time()
 
 txn = block_search(("164.132.229.117", 8333),
 				block_hash,
-                target_unspent_tx="82e63e889759bccc0561d771c657fb0e451bd3b750d5735535f8c770891c1a46")
-
-print(current_process.cpu_percent())
+                target_unspent_tx="013580abb1c034ade232da56e743299dca7e1372a44a95ea37b8673baf266198")
 
 now2 = time()
 
